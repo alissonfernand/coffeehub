c5f246efda8aca6d4d9c975649c94d7c430fd72a
alissonfernandes <alisson.flutter@gmail.com>
2021-03-02 20:03:20 -0300

    criado objetos de utilitarios

---
+++ src/consts/utils.js
@@ -0,0 +1,13 @@
+// COLORS
+export const COLORS = {
+  background: '#FAF4EE',
+  primary: 'rgba(207, 151,117, 1)',
+  tabBarActiveTintColor: 'rgba(207, 151,117, 1)',
+  tabBarInactiveTintColor: 'rgba(207, 151,117, 0.3)',
+};
+
+// FONT_FAMILY
+export const FONT_FAMILY = {
+  fontRegular: 'Sen-Regular',
+  fontBold: 'Sen-Bold',
+};
