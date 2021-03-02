d286013a7658b1b635f03946da9805bc16bd6ba3
alissonfernandes <alisson.flutter@gmail.com>
2021-03-02 20:04:01 -0300

    criado objeto coffee

---
+++ src/consts/coffees.js
@@ -0,0 +1,34 @@
+const coffees = [
+  {
+    id: 1,
+    name: 'Espresso',
+    price: 9,
+    image: require('../assets/coffees/Espresso.png'),
+  },
+  {
+    id: 2,
+    name: 'Cappuccino',
+    price: 9,
+    image: require('../assets/coffees/Cappuccino.png'),
+  },
+  {
+    id: 3,
+    name: 'Macchiato',
+    price: 9,
+    image: require('../assets/coffees/macciato.png'),
+  },
+  {
+    id: 4,
+    name: 'Mocha',
+    price: 9,
+    image: require('../assets/coffees/Mocha.png'),
+  },
+  {
+    id: 5,
+    name: 'Latte',
+    price: 9,
+    image: require('../assets/coffees/latte.png'),
+  },
+];
+
+export default coffees;
