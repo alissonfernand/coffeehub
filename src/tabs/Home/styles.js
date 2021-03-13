d6e7ef2890a9793e63da03f08bce5075082290b5
alissonfernandes <alisson.flutter@gmail.com>
2021-03-13 19:40:41 -0300

    adicionado components pra ficar responsivo

--- src/tabs/Home/styles.js
+++ src/tabs/Home/styles.js
@@ -2,6 +2,12 @@ import styled from 'styled-components/native';
 
 import Icon from 'react-native-vector-icons/FontAwesome';
 
+import {
+  responsiveHeight,
+  responsiveWidth,
+  responsiveFontSize,
+} from 'react-native-responsive-dimensions';
+
 import {COLORS, FONT_FAMILY} from '../../consts/utils';
 
 export const Container = styled.SafeAreaView`
@@ -11,21 +17,25 @@ export const Container = styled.SafeAreaView`
 
 export const NavBar = styled.View`
   flex-direction: row;
-  /* height: 60px; */
   justify-content: space-between;
   align-items: center;
 
-  padding: 27px 18px 32px;
+  padding: ${responsiveHeight(3.125)}px ${responsiveHeight(4.347826086956522)}px
+    ${responsiveHeight(3.58)}px;
 `;
 
 export const DisplayDrawer = styled.TouchableOpacity``;
 
 export const DisplaySearch = styled(DisplayDrawer)``;
 
-export const Image = styled.Image``;
+export const Image = styled.Image`
+  width: ${responsiveWidth(5.08)};
+  height: ${responsiveHeight(2.35)};
+`;
 
 export const Menu = styled.View`
-  padding: 0 18px 32px;
+  padding: 0 ${responsiveHeight(4.347826086956522)}px
+    ${responsiveHeight(3.58)}px;
 `;
 
 export const MenuRow = styled.View`
@@ -34,20 +44,19 @@ export const MenuRow = styled.View`
 
 export const Title = styled.Text`
   font-family: ${FONT_FAMILY.fontRegular};
-  font-size: 36px;
+  font-size: ${responsiveFontSize(4.5)};
 `;
 
 export const TitleBold = styled(Title)`
   font-family: ${FONT_FAMILY.fontBold};
   color: ${COLORS.primary};
-  max-width: 150px;
 `;
 
 export const FlatItem = styled.View`
   flex-direction: row;
   justify-content: space-between;
   align-items: center;
-  margin: 0 36px 70px;
+  margin: 0 ${responsiveHeight(4.347826086956522)}px ${responsiveHeight(6.7)}px;
 `;
 
 export const Wrapper = styled.View`
@@ -56,17 +65,17 @@ export const Wrapper = styled.View`
 `;
 
 export const IconCoffee = styled(Image)`
-  width: 40;
-  height: 60px;
-  margin-right: 32px;
+  width: ${responsiveWidth(9.67)};
+  height: ${responsiveHeight(6.7)};
+  margin-right: ${responsiveWidth(7.73)}px;
 `;
 
 export const CoffeeTitle = styled(Title)`
-  font-size: 20px;
-  line-height: 27;
+  font-size: ${responsiveFontSize(2.5)};
+  line-height: ${responsiveFontSize(3.375)};
 `;
 
 export const IconArrow = styled(Icon)`
   color: ${COLORS.primary};
-  font-size: 14px;
+  font-size: ${responsiveFontSize(1.75)};
 `;
