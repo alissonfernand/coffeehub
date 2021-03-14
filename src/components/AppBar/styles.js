a8537962690be49ea01ddd6a8a4e1d0d2c917c36
alissonfernandes <alisson.flutter@gmail.com>
2021-03-13 22:29:02 -0300

    correcao para centralizar os components

--- src/components/AppBar/styles.js
+++ src/components/AppBar/styles.js
@@ -5,23 +5,28 @@ import Icon from 'react-native-vector-icons/FontAwesome';
 import {FONT_FAMILY} from '../../consts/utils';
 
 export const AppBarContainer = styled.View`
+  position: relative;
   flex-direction: row;
-  align-items: center;
-  justify-content: space-between;
   width: 100%;
   padding: 65px 32px 25px;
   margin-bottom: 47px;
 `;
 
+export const HandleNavigation = styled.TouchableOpacity``;
+
 export const IconBack = styled(Icon).attrs({
   name: 'chevron-left',
 })`
+  position: absolute;
+  flex: 1;
+  top: 5;
   font-size: 20px;
   color: #8c746a;
 `;
 
 export const ProductName = styled.Text`
-  flex: 2;
+  flex: 1;
+  text-align: center;
   font-family: ${FONT_FAMILY.fontBold};
   font-size: 23px;
   line-height: 31;
