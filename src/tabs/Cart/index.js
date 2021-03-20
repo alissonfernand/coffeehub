176f826ddf1749c149251a2f5c5fe016be909235
alissonfernandes <alisson.flutter@gmail.com>
2021-03-20 19:05:54 -0300

    usando redux

--- src/tabs/Cart/index.js
+++ src/tabs/Cart/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import {StatusBar} from 'react-native';
 
+import {useSelector} from 'react-redux';
+
 import coffee from '../../assets/images/macciato.png';
 
 // import Button from '../../components/Button';
@@ -25,6 +27,8 @@ import {
 } from './styles';
 
 const Cart = () => {
+  // get state reducer
+  const {products} = useSelector((state) => state.cart);
   return (
     <Container>
       <StatusBar
@@ -36,103 +40,24 @@ const Cart = () => {
       <TextCard>Card</TextCard>
 
       <ScrollProducts showsVerticalScrollIndicator={false}>
-        <CartProduct>
-          <ProductImage source={coffee} />
+        {products.map((product) => (
+          <CartProduct>
+            <ProductImage source={coffee} />
 
-          <Wrapper>
-            <ProductDetails>
-              <ProductName>Macchiato</ProductName>
-              <ProductSize>Size Small</ProductSize>
-              <ProductSugar>Sugar One</ProductSugar>
-            </ProductDetails>
+            <Wrapper>
+              <ProductDetails>
+                <ProductName>{product.name}</ProductName>
+                <ProductSize>Size {product.size}</ProductSize>
+                <ProductSugar>Sugar {product.sugar}</ProductSugar>
+              </ProductDetails>
 
-            <ContainerQuantityPrice>
-              <ProductAmount>x2</ProductAmount>
-              <ProductPrice>$210</ProductPrice>
-            </ContainerQuantityPrice>
-          </Wrapper>
-        </CartProduct>
-        <CartProduct>
-          <ProductImage source={coffee} />
-
-          <Wrapper>
-            <ProductDetails>
-              <ProductName>Macchiato</ProductName>
-              <ProductSize>Size Small</ProductSize>
-              <ProductSugar>Sugar One</ProductSugar>
-            </ProductDetails>
-
-            <ContainerQuantityPrice>
-              <ProductAmount>x2</ProductAmount>
-              <ProductPrice>$210</ProductPrice>
-            </ContainerQuantityPrice>
-          </Wrapper>
-        </CartProduct>
-
-        <CartProduct>
-          <ProductImage source={coffee} />
-
-          <Wrapper>
-            <ProductDetails>
-              <ProductName>Macchiato</ProductName>
-              <ProductSize>Size Small</ProductSize>
-              <ProductSugar>Sugar One</ProductSugar>
-            </ProductDetails>
-
-            <ContainerQuantityPrice>
-              <ProductAmount>x2</ProductAmount>
-              <ProductPrice>$210</ProductPrice>
-            </ContainerQuantityPrice>
-          </Wrapper>
-        </CartProduct>
-        <CartProduct>
-          <ProductImage source={coffee} />
-
-          <Wrapper>
-            <ProductDetails>
-              <ProductName>Macchiato</ProductName>
-              <ProductSize>Size Small</ProductSize>
-              <ProductSugar>Sugar One</ProductSugar>
-            </ProductDetails>
-
-            <ContainerQuantityPrice>
-              <ProductAmount>x2</ProductAmount>
-              <ProductPrice>$210</ProductPrice>
-            </ContainerQuantityPrice>
-          </Wrapper>
-        </CartProduct>
-        <CartProduct>
-          <ProductImage source={coffee} />
-
-          <Wrapper>
-            <ProductDetails>
-              <ProductName>Macchiato</ProductName>
-              <ProductSize>Size Small</ProductSize>
-              <ProductSugar>Sugar One</ProductSugar>
-            </ProductDetails>
-
-            <ContainerQuantityPrice>
-              <ProductAmount>x2</ProductAmount>
-              <ProductPrice>$210</ProductPrice>
-            </ContainerQuantityPrice>
-          </Wrapper>
-        </CartProduct>
-        <CartProduct>
-          <ProductImage source={coffee} />
-
-          <Wrapper>
-            <ProductDetails>
-              <ProductName>Macchiato</ProductName>
-              <ProductSize>Size Small</ProductSize>
-              <ProductSugar>Sugar One</ProductSugar>
-            </ProductDetails>
-
-            <ContainerQuantityPrice>
-              <ProductAmount>x2</ProductAmount>
-              <ProductPrice>$210</ProductPrice>
-            </ContainerQuantityPrice>
-          </Wrapper>
-        </CartProduct>
+              <ContainerQuantityPrice>
+                <ProductAmount>x{product.amount}</ProductAmount>
+                <ProductPrice>${product.price}</ProductPrice>
+              </ContainerQuantityPrice>
+            </Wrapper>
+          </CartProduct>
+        ))}
       </ScrollProducts>
 
       {/* <ContainerPriceBuy>
