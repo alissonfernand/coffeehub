44a22392834deaf52fe2d85a62dc1a0c636c4f94
alissonfernandes <alisson.flutter@gmail.com>
2021-03-20 19:06:36 -0300

    add produtos no reducer

--- src/store/modules/cart/reducer.js
+++ src/store/modules/cart/reducer.js
@@ -3,20 +3,19 @@ import produce from 'immer';
 
 const INITIAL_STATE = {
   products: [],
-  product: {
-    name: null,
-    price: null,
-    size: null,
-    sugar: null,
-    amount: null,
-  },
 };
 
 function cart(state = INITIAL_STATE, action) {
   switch (action.type) {
     case types.ADD_TO_CART:
       return produce(state, (draft) => {
-        draft.products = [...state.products, ...action.product];
+        draft.products.push({
+          name: action.product.name,
+          price: action.product.price,
+          size: action.product.size,
+          sugar: action.product.sugar,
+          amount: action.product.amount,
+        });
       });
     default:
       return state;
