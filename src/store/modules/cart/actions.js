8d056ded2a16af102b2b31f2c46d5843843e131d
alissonfernandes <alisson.flutter@gmail.com>
2021-03-20 19:05:42 -0300

    passando params

--- src/store/modules/cart/actions.js
+++ src/store/modules/cart/actions.js
@@ -1,5 +1,5 @@
 import types from './types';
 
-export function addToCart() {
-  return {type: types.ADD_TO_CART};
+export function addToCart(product) {
+  return {type: types.ADD_TO_CART, product};
 }
