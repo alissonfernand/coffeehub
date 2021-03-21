1a99501e1a271ddc32a1d3fb9f95c4d25ecc38a6
alissonfernandes <alisson.flutter@gmail.com>
2021-03-21 13:08:28 -0300

    add product id e image

--- src/store/modules/cart/reducer.js
+++ src/store/modules/cart/reducer.js
@@ -10,11 +10,13 @@ function cart(state = INITIAL_STATE, action) {
     case types.ADD_TO_CART:
       return produce(state, (draft) => {
         draft.products.push({
+          id: action.product.id,
           name: action.product.name,
           price: action.product.price,
           size: action.product.size,
           sugar: action.product.sugar,
           amount: action.product.amount,
+          image: action.product.image,
         });
       });
     default:
