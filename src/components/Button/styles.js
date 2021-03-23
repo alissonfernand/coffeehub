5a99352add7464aa07d78c54dc3f1f4d37691569
alissonfernandes <alisson.flutter@gmail.com>
2021-03-22 23:27:43 -0300

    alterando elevation

--- src/components/Button/styles.js
+++ src/components/Button/styles.js
@@ -2,14 +2,12 @@ import styled from 'styled-components/native';
 
 import {COLORS} from '../../consts/utils';
 
-export const ButtonCart = styled.TouchableOpacity.attrs({
-  shadowcolor: 'red',
-})`
+export const ButtonCart = styled.TouchableOpacity.attrs({})`
   width: 198px;
   height: 55px;
   background-color: ${COLORS.primary};
   border-radius: 28px;
   justify-content: center;
   align-items: center;
-  elevation: 30;
+  elevation: 20;
 `;
