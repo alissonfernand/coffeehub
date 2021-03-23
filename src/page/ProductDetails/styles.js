00070954863768bee780840d8727e378972888d8
alissonfernandes <alisson.flutter@gmail.com>
2021-03-22 23:27:59 -0300

    add responsividade

--- src/page/ProductDetails/styles.js
+++ src/page/ProductDetails/styles.js
@@ -1,5 +1,11 @@
 import styled from 'styled-components/native';
 
+import {
+  responsiveHeight,
+  responsiveWidth,
+  responsiveFontSize,
+} from 'react-native-responsive-dimensions';
+
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 import {COLORS, FONT_FAMILY} from '../../consts/utils';
@@ -8,11 +14,12 @@ export const Container = styled.View`
   flex: 1;
 `;
 
-export const ContainerShadow = styled.View`
-  height: 369px;
+export const ContainerShadow = styled.View.attrs({
+  elevation: 30,
+})`
+  height: ${responsiveHeight(44.19642857142857)}px;
   border-radius: 46px;
   background-color: #faf4ee;
-  elevation: 20;
 `;
 
 export const ImageBackground = styled.ImageBackground.attrs({
@@ -21,18 +28,18 @@ export const ImageBackground = styled.ImageBackground.attrs({
   borderBottomRightRadius: 46,
   backgroundColor: '#FAF4EE',
 })`
-  width: 100%;
-  height: 369px;
+  width: ${responsiveWidth(100)}px;
+  height: ${responsiveHeight(46)}px;
   align-items: center;
-  /* padding: 0 27px; */
 `;
 
 export const AppBar = styled.View`
-  width: 100%;
+  width: ${responsiveWidth(100)}px;
   flex-direction: row;
   justify-content: space-between;
   align-items: center;
-  padding: 60px 32px 47px;
+  padding: ${responsiveHeight(9.5)}px ${responsiveWidth(7.729468599033816)}px
+    ${responsiveHeight(5.245535714285714)}px;
 `;
 
 export const HandleNavigation = styled.TouchableOpacity``;
@@ -40,44 +47,45 @@ export const HandleNavigation = styled.TouchableOpacity``;
 export const IconBack = styled(Icon).attrs({
   name: 'chevron-left',
 })`
-  font-size: 20px;
+  font-size: ${responsiveFontSize(3) - 4}px;
   color: #8c746a;
 `;
 
 export const AppBarTitle = styled.Text`
   font-family: ${FONT_FAMILY.fontBold};
-  font-size: 23px;
-  line-height: 31px;
+  font-size: ${responsiveFontSize(3) - 1}px;
+  line-height: ${responsiveHeight(3.459821428571429)}px;
   text-align: center;
 `;
 
 export const Spacer = styled.View`
-  width: 20px;
-  height: 20px;
+  width: ${responsiveWidth(4.830917874396135)}px;
+  height: ${responsiveHeight(2.232142857142857)}px;
 `;
 
 export const ProductImage = styled.Image.attrs({
   resizeMode: 'contain',
 })`
-  width: 86px;
-  height: 128px;
+  width: ${responsiveWidth(20.77294685990338)}px;
+  height: ${responsiveHeight(14.28571428571429)}px;
 `;
 
 export const Wrapper = styled.View`
   flex-direction: row;
   justify-content: space-between;
-  margin: 40px 24px 35px;
+  margin: ${responsiveHeight(4.464285714285714)}px
+    ${responsiveWidth(5.797101449275362)}px ${responsiveHeight(3.90625)}px;
 `;
 export const ProducName = styled.Text`
   font-family: ${FONT_FAMILY.fontRegular};
-  font-size: 20px;
-  line-height: 27px;
+  font-size: ${responsiveFontSize(3) - 4}px;
+  line-height: ${responsiveHeight(3.013392857142857)}px;
   color: #2d140d;
 `;
 export const ProducPrice = styled.Text`
   font-family: ${FONT_FAMILY.fontBold};
-  font-size: 30px;
-  line-height: 40px;
+  font-size: ${responsiveFontSize(4) - 2}px;
+  line-height: ${responsiveHeight(4.464285714285714)}px;
   color: ${COLORS.primary};
 `;
 
@@ -90,8 +98,8 @@ export const ContainerAmount = styled.View`
 export const Decrement = styled.TouchableOpacity.attrs({
   activeOpacity: 0.6,
 })`
-  height: 28px;
-  width: 31px;
+  height: ${responsiveHeight(3.125)}px;
+  width: ${responsiveWidth(7.48792270531401)}px;
   justify-content: center;
   align-items: center;
   text-align: center;
@@ -111,7 +119,7 @@ export const MinusIcon = styled(IconPlus).attrs({
 })``;
 
 export const Amount = styled.Text`
-  width: 34px;
+  width: ${responsiveWidth(8.21256038647343)}px;
   text-align: center;
 `;
 
@@ -120,8 +128,8 @@ export const Increment = styled.TouchableOpacity.attrs({
 })`
   justify-content: center;
   align-items: center;
-  height: 28px;
-  width: 31px;
+  height: ${responsiveHeight(3.125)}px;
+  width: ${responsiveWidth(7.48792270531401)}px;
   background: ${COLORS.primary};
   border-top-right-radius: 25px;
   border-bottom-right-radius: 25px;
@@ -129,19 +137,19 @@ export const Increment = styled.TouchableOpacity.attrs({
 
 export const TextSize = styled.Text`
   font-family: ${FONT_FAMILY.fontRegular};
-  font-size: 20px;
-  line-height: 27px;
+  font-size: ${responsiveFontSize(3) - 4}px;
+  line-height: ${responsiveHeight(3.013392857142857)}px;
   color: #2d140d;
-  padding-left: 24px;
+  padding-left: ${responsiveWidth(5.797101449275362)}px;
 `;
 
 export const WrapperIcons = styled.View`
-  width: 45%;
+  width: ${responsiveWidth(45)}px;
   flex-direction: row;
   justify-content: space-between;
   align-items: flex-end;
-  padding-left: 24px;
-  margin-bottom: 35px;
+  padding-left: ${responsiveWidth(5.797101449275362)}px;
+  margin-bottom: ${responsiveHeight(3.90625)}px;
 `;
 
 export const SelectCoffee = styled.TouchableOpacity`
@@ -149,50 +157,50 @@ export const SelectCoffee = styled.TouchableOpacity`
 `;
 
 export const BorderBottom = styled.View`
-  width: 14px;
-  height: 2px;
+  width: ${responsiveWidth(3.381642512077295)}px;
+  height: ${responsiveHeight(0.2232142857142857)}px;
   border-radius: 50px;
   background: ${COLORS.primary};
   background: ${(props) => (props.isActive ? COLORS.primary : 'transparent')};
-  margin-top: 13px;
+  margin-top: ${responsiveHeight(1.450892857142857)}px;
 `;
 
 export const ContainerSugar = styled.View`
   flex-direction: row;
-  padding-left: 24px;
-  margin-bottom: 9px;
+  padding-left: ${responsiveWidth(5.797101449275362)}px;
+  margin-bottom: ${responsiveHeight(1.004464285714286)}px;
 `;
 
 export const TextSugar = styled.Text`
   font-family: ${FONT_FAMILY.fontRegular};
-  font-size: 20px;
-  line-height: 27px;
+  font-size: ${responsiveFontSize(3) - 4}px;
+  line-height: ${responsiveHeight(3.013392857142857)}px;
   color: #2d140d;
 `;
 
 export const InCubes = styled(TextSugar)`
   opacity: 0.3;
-  margin-left: 11px;
+  margin-left: ${responsiveWidth(2.657004830917874)}px;
 `;
 
 export const ContainerSugarIcons = styled.View`
-  width: 65%;
+  width: ${responsiveWidth(65)}px;
   flex-direction: row;
   justify-content: space-between;
   align-items: flex-end;
-  padding-left: 24px;
-  margin-bottom: 39px;
+  padding-left: ${responsiveWidth(5.797101449275362)}px;
+  margin-bottom: ${responsiveHeight(4.352678571428571)}px;
 `;
 
 export const SelectSugar = styled(SelectCoffee)``;
 
 export const ContainerButton = styled.View`
   align-items: center;
-  margin-bottom: 55px;
+  margin-bottom: ${responsiveHeight(6.138392857142857)}px;
 `;
 
 export const TextButton = styled.Text.attrs({})`
   font-family: ${FONT_FAMILY.fontRegular};
-  font-size: 18px;
+  font-size: ${responsiveFontSize(2) + 2}px;
   color: #ffffff;
 `;
